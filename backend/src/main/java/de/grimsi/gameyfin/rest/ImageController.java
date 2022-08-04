1879648e2537cd19d539d9f63404790bf51b9895
grimsi <9295182+grimsi@users.noreply.github.com>
2022-08-04 15:57:40 +0200

    Implemented caching of static images

--- backend/src/main/java/de/grimsi/gameyfin/rest/ImageController.java
+++ backend/src/main/java/de/grimsi/gameyfin/rest/ImageController.java
@@ -3,11 +3,15 @@ package de.grimsi.gameyfin.rest;
 import de.grimsi.gameyfin.service.DownloadService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.Resource;
+import org.springframework.http.CacheControl;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * This controller handles functionality for images.
@@ -20,7 +24,9 @@ public class ImageController {
     private final DownloadService downloadService;
 
     @GetMapping(value = "/{imageId}", produces = MediaType.IMAGE_PNG_VALUE)
-    public Resource getCoverImageForGame(@PathVariable String imageId) {
-        return downloadService.downloadImage(imageId);
+    public ResponseEntity<Resource> getCoverImageForGame(@PathVariable String imageId) {
+        return ResponseEntity.ok()
+                .cacheControl(CacheControl.maxAge(365, TimeUnit.DAYS).cachePublic())
+                .body(downloadService.downloadImage(imageId));
     }
 }
