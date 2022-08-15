291d7ad8d8aaf11bec98c5b4d815067cb6711eeb
grimsi <9295182+grimsi@users.noreply.github.com>
2022-08-15 17:30:04 +0200

    Fixed "Go to library screen" shown on library screen when filters were active

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -51,7 +51,7 @@ export class HeaderComponent {
   }
 
   onLibraryScreen(): boolean {
-    return this.router.url === "/library";
+    return this.router.url.startsWith("/library");
   }
 
   onLibraryManagementScreen(): boolean {
