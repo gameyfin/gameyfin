29bb7f42144e4d631141d04bb92901d68170f8b5
tr7zw <tr7zw@live.de>
2022-10-30 20:41:39 +0100

    Validate some combinations of filename with added suffixes
    Also fixes a bug of not removing trailing empty [].

--- backend/src/test/java/de/grimsi/gameyfin/util/FilenameUtilTest.java
+++ backend/src/test/java/de/grimsi/gameyfin/util/FilenameUtilTest.java
@@ -1,27 +1,26 @@
 package de.grimsi.gameyfin.util;
 
-import com.google.common.jimfs.Configuration;
-import com.google.common.jimfs.Jimfs;
-import org.apache.commons.io.FileUtils;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Named.named;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.io.IOException;
-import java.nio.file.FileSystem;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.List;
-import java.util.stream.Stream;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Named.named;
-import static org.junit.jupiter.params.provider.Arguments.arguments;
+import com.google.common.jimfs.Configuration;
+import com.google.common.jimfs.Jimfs;
 
 class FilenameUtilTest {
 
@@ -30,10 +29,13 @@ class FilenameUtilTest {
     private static final FileSystem winFS = Jimfs.newFileSystem(Configuration.windows());
 
     private static final List<String> gameFileExtensions = List.of("extension_1", "extension_2", "extension_3");
+    private static final List<String> possibleGameFileSuffixes = Arrays.asList("windows, win, english, win32, win64, opengl, stable".split(", "));
 
     @BeforeAll
     static void init() {
-        new FilenameUtil().setPossibleGameFileExtensions(gameFileExtensions);
+        FilenameUtil filenameUtil = new FilenameUtil();
+        filenameUtil.setPossibleGameFileExtensions(gameFileExtensions);
+        filenameUtil.setPossibleGameFileSuffixes(possibleGameFileSuffixes);
     }
 
     @AfterAll
@@ -113,12 +115,60 @@ class FilenameUtilTest {
 
         Files.deleteIfExists(p);
     }
+    
+    @ParameterizedTest
+    @MethodSource("exampleFilenames")
+    void removeFileSuffixes(String filename) {
+        assertEquals(filename, FilenameUtil.getFilenameWithoutAdditions(unixFS.getPath("%s.%s".formatted(filename, "-win"))));
+        assertEquals(filename, FilenameUtil.getFilenameWithoutAdditions(unixFS.getPath("%s.%s".formatted(filename, "-v1.05.4"))));
+        assertEquals(filename, FilenameUtil.getFilenameWithoutAdditions(unixFS.getPath("%s.%s".formatted(filename, "-win32"))));
+        assertEquals(filename, FilenameUtil.getFilenameWithoutAdditions(unixFS.getPath("%s.%s".formatted(filename, "-win-opengl(windows)"))));
+        assertEquals(filename, FilenameUtil.getFilenameWithoutAdditions(unixFS.getPath("%s.%s".formatted(filename, "-windows-stable"))));
+        assertEquals(filename, FilenameUtil.getFilenameWithoutAdditions(unixFS.getPath("%s.%s".formatted(filename, "[windows]"))));
+        assertEquals(filename, FilenameUtil.getFilenameWithoutAdditions(unixFS.getPath("%s.%s".formatted(filename, "[stable]"))));
+        assertEquals(filename, FilenameUtil.getFilenameWithoutAdditions(unixFS.getPath("%s.%s".formatted(filename, "(opengl)"))));
+    }
+    
+    @ParameterizedTest
+    @MethodSource("exampleFilenames")
+    void removeFileSuffixesFileExtensions(String filename) {
+        assertEquals(filename, FilenameUtil.getFilenameWithoutAdditions(unixFS.getPath("%s.%s.%s".formatted(filename, "-win", gameFileExtensions.get(0)))));
+        assertEquals(filename, FilenameUtil.getFilenameWithoutAdditions(unixFS.getPath("%s.%s.%s".formatted(filename, "-v1.05.4", gameFileExtensions.get(0)))));
+        assertEquals(filename, FilenameUtil.getFilenameWithoutAdditions(unixFS.getPath("%s.%s.%s".formatted(filename, "-win32", gameFileExtensions.get(0)))));
+        assertEquals(filename, FilenameUtil.getFilenameWithoutAdditions(unixFS.getPath("%s.%s.%s".formatted(filename, "-win-opengl(windows)", gameFileExtensions.get(0)))));
+        assertEquals(filename, FilenameUtil.getFilenameWithoutAdditions(unixFS.getPath("%s.%s.%s".formatted(filename, "-windows-stable", gameFileExtensions.get(0)))));
+        assertEquals(filename, FilenameUtil.getFilenameWithoutAdditions(unixFS.getPath("%s.%s.%s".formatted(filename, "[windows]", gameFileExtensions.get(0)))));
+        assertEquals(filename, FilenameUtil.getFilenameWithoutAdditions(unixFS.getPath("%s.%s.%s".formatted(filename, "[stable]", gameFileExtensions.get(0)))));
+        assertEquals(filename, FilenameUtil.getFilenameWithoutAdditions(unixFS.getPath("%s.%s.%s".formatted(filename, "(opengl)", gameFileExtensions.get(0)))));
+    }
+    
+    @ParameterizedTest
+    @MethodSource("exampleFilenames")
+    void removeFileSuffixesWithAddedSpaces(String filename) {
+        assertEquals(filename, FilenameUtil.getFilenameWithoutAdditions(unixFS.getPath("  %s.%s   .%s".formatted(filename, "-win", gameFileExtensions.get(0)))));
+        assertEquals(filename, FilenameUtil.getFilenameWithoutAdditions(unixFS.getPath("  %s.%s     .%s".formatted(filename, "-v1.05.4", gameFileExtensions.get(0)))));
+        assertEquals(filename, FilenameUtil.getFilenameWithoutAdditions(unixFS.getPath(" %s.%s .%s".formatted(filename, "-win32", gameFileExtensions.get(0)))));
+        assertEquals(filename, FilenameUtil.getFilenameWithoutAdditions(unixFS.getPath(" %s.%s   .%s".formatted(filename, "-win-opengl(windows)", gameFileExtensions.get(0)))));
+        assertEquals(filename, FilenameUtil.getFilenameWithoutAdditions(unixFS.getPath(" %s.%s  .%s".formatted(filename, "-windows-stable", gameFileExtensions.get(0)))));
+        assertEquals(filename, FilenameUtil.getFilenameWithoutAdditions(unixFS.getPath(" %s.%s  .%s".formatted(filename, "[windows]", gameFileExtensions.get(0)))));
+        assertEquals(filename, FilenameUtil.getFilenameWithoutAdditions(unixFS.getPath(" %s.%s  .%s".formatted(filename, "[stable]", gameFileExtensions.get(0)))));
+        assertEquals(filename, FilenameUtil.getFilenameWithoutAdditions(unixFS.getPath(" %s.%s  .%s".formatted(filename, "(opengl)", gameFileExtensions.get(0)))));
+    }
+
 
     private static Stream<Arguments> fileSystems() {
         return Stream.of(
                 arguments(named("Unix", unixFS)),
                 arguments(named("OSX", osxFS)),
                 arguments(named("Windows", winFS))
+        );
+    }
+    
+    private static Stream<Arguments> exampleFilenames() {
+        return Stream.of(
+                arguments(named("example_file", "example_file")),
+                arguments(named("example-file", "example-file")),
+                arguments(named("example file", "example file"))
         );
     }
 }
