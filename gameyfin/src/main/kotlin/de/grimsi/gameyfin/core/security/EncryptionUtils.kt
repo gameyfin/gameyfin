c2c6a891b9ea2c7d0ad7a7e79695703b4054a70c
grimsi <9295182+grimsi@users.noreply.github.com>
2025-05-27 17:58:19 +0200

    Validate APP_KEY at startup

--- gameyfin/src/main/kotlin/de/grimsi/gameyfin/core/security/EncryptionUtils.kt
+++ gameyfin/src/main/kotlin/de/grimsi/gameyfin/core/security/EncryptionUtils.kt
@@ -1,6 +1,6 @@
 package de.grimsi.gameyfin.core.security
 
-import java.util.Base64
+import java.util.*
 import javax.crypto.Cipher
 import javax.crypto.spec.SecretKeySpec
 
@@ -12,14 +12,7 @@ class EncryptionUtils {
         init {
             val base64Key = System.getenv("APP_KEY")
                 ?: throw IllegalStateException("APP_KEY environment variable is not set or empty")
-
             val decodedKey = Base64.getDecoder().decode(base64Key)
-
-            // Ensure the key length is valid for AES (128, 192, or 256 bits)
-            if (decodedKey.size !in listOf(16, 24, 32)) {
-                throw IllegalArgumentException("Invalid AES key length. Key must be 128, 192, or 256 bits.")
-            }
-
             SECRET_KEY = SecretKeySpec(decodedKey, ALGORITHM)
         }
 
