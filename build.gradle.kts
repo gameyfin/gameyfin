06ee89656d6ab6d8b73c0b19e7c9cbeef58fff0e
grimsi <9295182+grimsi@users.noreply.github.com>
2026-03-07 11:35:20 +0100

    Revert incorrect version update

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 import java.nio.file.Files
 
 group = "org.gameyfin"
-version = "856-malformed-external-links-preview"
+version = "2.4.0-preview"
 
 allprojects {
     repositories {
